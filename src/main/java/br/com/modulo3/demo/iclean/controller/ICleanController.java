948138d30eba9f905bf8317bbf215d43b4303ccb
jpiovani <jpiovani@outlook.com>
2021-10-16 14:14:56 -0300

    Politica CORS

--- src/main/java/br/com/modulo3/demo/iclean/controller/ICleanController.java
+++ src/main/java/br/com/modulo3/demo/iclean/controller/ICleanController.java
@@ -4,18 +4,14 @@ import br.com.modulo3.demo.iclean.dto.LavanderiaDTO;
 import br.com.modulo3.demo.iclean.dto.UsuarioDTO;
 
 import br.com.modulo3.demo.iclean.service.ICleanService;
-import io.swagger.v3.oas.annotations.Parameter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.PushBuilder;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
-@RequestMapping("lavanderias")
+@RequestMapping("lavanderias/v1")
 public class ICleanController {
 
     private ICleanService service;
@@ -24,6 +20,7 @@ public class ICleanController {
         this.service = service;
     }
 
+    @CrossOrigin
     @GetMapping
     public List<LavanderiaDTO> getLavanderiaByBairro(
             @RequestParam(required = false, value = "bairroLavanderia") String bairroLavanderia){
@@ -32,12 +29,14 @@ public class ICleanController {
 
     }
 
+    @CrossOrigin
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public LavanderiaDTO criar(@RequestBody LavanderiaDTO nova){
         return service.criarLavanderia(nova);
     }
 
+    @CrossOrigin
     @PostMapping(value = "/usuarios")
     @ResponseStatus(HttpStatus.CREATED)
     public UsuarioDTO create(@RequestBody UsuarioDTO novo){
